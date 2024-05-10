5d5b9f355766a074604b8391fdb7688423940013
Dillion Megida <dillionmegida@gmail.com>
2024-05-10 15:50:29 +0200

    add auto import for CodeBlock in mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,28 @@
-import mdx from "@astrojs/mdx";
-import { defineConfig } from "astro/config";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx"
+import { defineConfig } from "astro/config"
+import partytown from "@astrojs/partytown"
+import sitemap from "@astrojs/sitemap"
+import AutoImport from 'astro-auto-import';
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://simpletypescript.dev",
   markdown: {
-    syntaxHighlight: "prism"
+    syntaxHighlight: "prism",
   },
-  integrations: [mdx(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), tailwind()]
-});
+  integrations: [
+    sitemap(),
+    AutoImport({
+      imports: ['./src/components/mdx/CodeBlock.astro']
+    }),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    tailwind(),
+  ],
+})
